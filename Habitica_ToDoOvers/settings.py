403c16d1a5ffd5f232feb6d3a7962e1f40bc5cbf
tomekbielaszewski <tomekbielaszewski@gmail.com>
2025-08-23 15:47:20 +0200

    css fixed in production setting (debug false)

--- Habitica_ToDoOvers/settings.py
+++ Habitica_ToDoOvers/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -105,3 +106,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
